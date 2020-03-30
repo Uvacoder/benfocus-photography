5b3f340b9386a246efdd459eda705d693d97ab04
Chandu J S <app.coder@outlook.in>
2020-03-30 13:27:25 +0530

    basic layout set

--- src/layouts/wrap.js
+++ src/layouts/wrap.js
@@ -1,3 +1,14 @@
-import React from 'react'
+/** @jsx jsx */
 
-export default () => <div></div>
+import { css, jsx } from '@emotion/core'
+
+import '../assets/css/style.css'
+
+export default ({ children }) => (
+  <div
+    css={css`
+      display: block;
+    `}>
+    {children}
+  </div>
+)
--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,6 @@ import Wrap from '../layouts/wrap'
 
 export default () => (
   <Wrap>
-    <h1>Hello!</h1>
+    <h1>404</h1>
   </Wrap>
 )
