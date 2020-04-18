5d4b8050f71fb11ece50b6407ccc2c35f8c8db74
Chandu J S <app.coder@outlook.in>
2020-04-18 10:16:27 +0530

    contact page done

--- src/components/site_button.js
+++ src/components/site_button.js
@@ -5,18 +5,30 @@ import { css, jsx } from '@emotion/core'
 
 import { Fonts, Colors } from '../constants'
 
-export default ({ to, noPadding, color, textColor, label }) => {
-  return (
+export default ({ to, noPadding, color, textColor, label, submit }) => {
+  const style = `font-family: ${Fonts.head};
+  background-color: ${color || Colors.white};
+  text-transform: uppercase;
+  padding: 1rem ${noPadding ? 0 : 2}rem;
+  letter-spacing: 0.05rem;
+  color: ${textColor || Colors.text};
+  display: inline-block;`
+  return submit ? (
+    <button
+      type="submit"
+      css={css`
+        ${style}
+        border: 0;
+        font-size: inherit;
+        outline: none !important;
+      `}>
+      {label}
+    </button>
+  ) : (
     <Link
       to={to}
       css={css`
-        font-family: ${Fonts.head};
-        background-color: ${color || Colors.white};
-        text-transform: uppercase;
-        padding: 1rem ${noPadding ? 0 : 2}rem;
-        letter-spacing: 0.05rem;
-        color: ${textColor || Colors.text};
-        display: inline-block;
+        ${style}
       `}>
       {label}
     </Link>
---
+++ src/pages/contact.js
@@ -0,0 +1,109 @@
+/** @jsx jsx */
+
+import { css, jsx } from '@emotion/core'
+
+import { Colors, Fonts } from '../constants'
+
+import Wrap from '../layouts/wrap'
+import SiteButton from '../components/site_button'
+import NormalSection from '../components/normal_section'
+import { SectionHead, SectionDescription } from '../components/section_data'
+
+export default () => (
+  <Wrap pageTitle="Contact Us" innerPageName="Contact Us">
+    <NormalSection color={Colors.primary}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <SectionHead color={Colors.white} title="Send Us A Message" />
+          </div>
+        </div>
+        <div
+          className="row"
+          css={css`
+            .input {
+              display: block;
+              width: 100%;
+              border: 0;
+              background-color: rgba(255, 255, 255, 0.9);
+              padding: 1rem;
+              font-size: inherit;
+              margin-bottom: 1rem;
+              outline: none !important;
+              transition-duration: 0.2s;
+              &:focus {
+                background-color: #fff;
+              }
+            }
+          `}>
+          <div className="col-4">
+            <input type="text" className="input" placeholder="Your Name" />
+          </div>
+          <div className="col-4">
+            <input type="text" className="input" placeholder="Your Email" />
+          </div>
+          <div className="col-8">
+            <textarea
+              rows="5"
+              css={css`
+                margin-bottom: 2rem !important;
+              `}
+              className="input"
+              placeholder="Your Message"></textarea>
+            <SiteButton
+              submit
+              color={Colors.secondary}
+              textColor={Colors.white}
+              label="Submit"
+            />
+          </div>
+        </div>
+      </div>
+    </NormalSection>
+    <NormalSection>
+      <div className="container">
+        <div className="row">
+          {[
+            {
+              title: 'Address',
+              body: ['Address Line One', 'Line Two', 'Country'],
+            },
+            {
+              title: 'Email',
+              body: ['benjtambara@gmail.com', 'main@ben-focus.com'],
+            },
+            {
+              title: 'Phone',
+              body: ['+248 2516575'],
+            },
+          ].map(item => {
+            return (
+              <div className="col-4">
+                <div
+                  css={css`
+                    color: ${Colors.primary};
+                    font-family: ${Fonts.head};
+                    margin-bottom: 2rem;
+                    font-size: 2rem;
+                    text-transform: uppercase;
+                  `}>
+                  {item.title}
+                </div>
+                <SectionDescription>
+                  {item.body.map(det => (
+                    <div
+                      css={css`
+                        margin-bottom: 0.1rem;
+                      `}>
+                      {det}
+                    </div>
+                  ))}
+                </SectionDescription>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </NormalSection>
+  </Wrap>
+)
